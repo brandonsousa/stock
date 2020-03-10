4820a22c22928ff138885687faef3c7dee109a39
Brandon Sousa da Silva <brandoncds10@gmail.com>
2020-03-10 18:39:53 -0300

    delete model clientAdresses cause client cant have one address

--- app/Models/ClientAddress.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
-const Model = use('Model')
-
-class ClientAddress extends Model {
-}
-
-module.exports = ClientAddress
